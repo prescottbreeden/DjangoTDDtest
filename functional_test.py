90e2d24e2c6c6af80e4b15fa0a99d2e1d4e323cb
prescott breeden <pb@prescotts-iMac.local>
2018-03-18 01:13:08 -0700

    class defined

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,36 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+
+class HomePageTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_starting_a_todo_list(self):
+		# Edith has heard about a cool new online to-do app.
+		# She goes to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		#She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+
+		# She is invited to enter todo item straight away
+
+		# She is invited to enter a to-do item straight away
+
+		# She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+
+		# when she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
+
+		# There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+		# The page updates again, and now shows both items on her list
+
+if __name__ == '__main__':
+	unittest.main(warnings="ignore")
